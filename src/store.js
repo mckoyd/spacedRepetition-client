b91e9dc0c48c8dfc93137b5592137ae1d988a27b
mckoyd <dominick.mckoy@gmail.com>
2018-07-09 16:26:12 -0400

    Client side JWT working

--- src/store.js
+++ src/store.js
@@ -7,20 +7,21 @@ import protectedDataReducer from './reducers/protected-data';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
 
 const store = createStore(
-    combineReducers({
-        form: formReducer,
-        auth: authReducer,
-        protectedData: protectedDataReducer
-    }),
-    applyMiddleware(thunk)
+  combineReducers({
+    form: formReducer,
+    auth: authReducer,
+    protectedData: protectedDataReducer
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
 // Hydrate the authToken from localStorage if it exist
 const authToken = loadAuthToken();
 if (authToken) {
-    const token = authToken;
-    store.dispatch(setAuthToken(token));
-    store.dispatch(refreshAuthToken());
+  const token = authToken;
+  store.dispatch(setAuthToken(token));
+  store.dispatch(refreshAuthToken());
 }
 
 export default store;
