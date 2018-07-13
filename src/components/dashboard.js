55c0f1db2657098c52c7d512a89b7f5b190249aa
mckoyd <dominick.mckoy@gmail.com>
2018-07-13 14:58:32 -0400

    Added percentage right | icon and title

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -49,8 +49,11 @@ export class Dashboard extends React.Component {
           <button disabled={this.props.pristine || this.props.submitting}>
             {this.props.buttonText}
           </button>
-          <h4>{this.props.answer===this.props.protectedData.enWord ? 'That\'s right, this is a ' + this.props.protectedData.enWord + ' in English! Move on the next animal...' :
-            this.props.answer==='' ? null : 'Sorry this is a ' + this.props.protectedData.enWord + ' in English! Try the next animal...' }</h4>
+          <h4>{this.props.answer===this.props.protectedData.enWord ? 'That\'s right, this is a ' + this.props.protectedData.enWord + ' in English! Move on the next animal...' 
+          + 'You\'re percentage right for this question is ' +  ((this.props.protectedData.timesCorrect + 1)/(this.props.protectedData.timesAnswered + 1)*100).toPrecision(2) + '%.':
+            this.props.answer==='' ? null : 'Sorry this is a ' + this.props.protectedData.enWord + ' in English! Try the next animal...'
+            + 'You\'re percentage right for this question is ' +  ((this.props.protectedData.timesCorrect)/(this.props.protectedData.timesAnswered + 1)*100).toPrecision(2) + '%.'
+          }</h4>
         </form>
       </div>
     );
